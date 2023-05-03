7e26f0edf2a4b75ea1b0bec1c2e51965c7ef88a7
Paulo Rubinger <paulopimentarubinger@gmail.com>
2023-05-02 21:18:02 -0300

    Fisrt Commit

---
+++ src/activity6/App.java
@@ -0,0 +1,10 @@
+package activity6;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        Jogador jogador = new Jogador();
+        jogador.leitura();
+    }
+}
