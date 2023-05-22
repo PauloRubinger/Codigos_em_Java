2e67702037c725e17c1c149f6d3d750d2209f06e
Paulo Rubinger <paulopimentarubinger@gmail.com>
2023-05-22 03:31:37 -0300

    Adicionando arquivos de log

--- SelectionSort/src/selectionsort/App.java
+++ SelectionSort/src/selectionsort/App.java
@@ -6,17 +6,21 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Jogador jogador = new Jogador();
-        SelectionSort mergesort = new SelectionSort();
+        SelectionSort selectionSort = new SelectionSort();
         ArrayList<Jogador> arrayOfPlayers;
 
         try {
             arrayOfPlayers = jogador.leitura();
-            arrayOfPlayers = mergesort.sort(arrayOfPlayers);
+
+            long startTime = System.currentTimeMillis();
+            arrayOfPlayers = selectionSort.sort(arrayOfPlayers);
+            long endTime = System.currentTimeMillis();
+            
+            selectionSort.arquivoLog(startTime, endTime);
 
             for (int i = 0; i < arrayOfPlayers.size(); i++) {
                 arrayOfPlayers.get(i).imprimir();
             }
-    
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- SelectionSort/src/selectionsort/SelectionSort.java
+++ SelectionSort/src/selectionsort/SelectionSort.java
@@ -1,8 +1,12 @@
 package selectionsort;
 
 import java.util.ArrayList;
+import java.io.*;
 
 public class SelectionSort {
+
+    int comp = 0;
+    int changes = 0;
 
     public SelectionSort() {
 
@@ -14,12 +18,30 @@ public class SelectionSort {
             for (int j = (i + 1); j < arrayOfPlayers.size(); j++) {
                 if (arrayOfPlayers.get(menor).getNome().compareTo(arrayOfPlayers.get(j).getNome()) > 0) {
                     menor = j;
+                    comp++;
                 }
             }
             Jogador temp = arrayOfPlayers.get(i);
             arrayOfPlayers.set(i, arrayOfPlayers.get(menor));
             arrayOfPlayers.set(menor, temp);
+            changes++;
         }
         return arrayOfPlayers;
+    }
+
+    // Criação do arquivo de log conforme pedido no enunciado para analisar o número de comparações e trocas feitas pelo método de ordenação, além do tempo de execução
+    public void arquivoLog(long startTime, long endTime) throws Exception{
+
+        FileWriter file = new FileWriter("794310_selecao.txt");
+        PrintWriter log = new PrintWriter(file);
+        long timeElapsed = endTime - startTime;
+        
+        log.printf("794310");
+        log.printf("\tTempo de execução(ms):" + timeElapsed);
+        log.printf("\tComparações feitas:" + comp);
+        log.printf("\tMovimentações feitas:" + changes);
+
+        log.close();
+        file.close();
     }
 }
